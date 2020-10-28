97788bf6e023d9637a0ebfb2ee858f119e698a0e
iii <izgitiii@optimisticlock.com>
2020-10-28 16:25:43 -0700

    Some input

--- wire.js
+++ wire.js
@@ -1,14 +1,17 @@
-
-
 class Edge {
-    constructor(from, to) {
-        this.from = from;
-        this.to = to;
+    constructor(x0, y0, x1, y1) {
+        this.x0 = x0;
+        this.y0 = y0;
+        this.x1 = x1;
+        this.y1 = y1;
     }
 }
 
-class Segment extends Edge {}
-class QuarterCircleArc extends Edge {}
+class Line extends Edge {
+}
+
+class Arc extends Edge {
+}
 
 class Wire {
 
@@ -18,4 +21,18 @@ class Wire {
     }
 }
 
+
+let wire = new Wire([
+    ["Line", 0, 1, 0, 9],
+    ["Arc", 0, 1, 1, 0],
+    ["Line", 1, 0, 9, 0],
+    ["Arc", 0, 9, 1, 10],
+    ["Line", 1, 10, 9, 10],
+    ["Arc", 9, 0, 10, 1],
+    ["Line", 10, 1, 10, 9],
+    ["Arc", 9, 10, 10, 9]
+]);
+
+
+// wire.constructor.name
 
