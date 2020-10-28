def1007e8b87704d5f77ab06be70ce88a16b7699
iii <izgitiii@optimisticlock.com>
2020-10-28 16:08:32 -0700

    rudimental Wire

--- wire.js
+++ wire.js
@@ -6,3 +6,16 @@ class Edge {
         this.to = to;
     }
 }
+
+class Segment extends Edge {}
+class QuarterCircleArc extends Edge {}
+
+class Wire {
+
+    // TODO verify that edges form a closed loop
+    constructor(edges) {
+        this.edges = edges;
+    }
+}
+
+
