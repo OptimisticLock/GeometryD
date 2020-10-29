84abe4aa0d914ee30325fa8c2bef10a8b84c2669
iii <izgitiii@optimisticlock.com>
2020-10-28 18:58:57 -0700

    beginning to generate svg arcs

--- render.js
+++ render.js
@@ -17,17 +17,28 @@ let wire = Wire.deserialize(wire0.serialize())
 function onload() {
     let g = document.getElementById("g");
 
-    for ([type, x1, y1, x2, y2] of wire.edges) {
+    let arc = document.createElementNS('http://www.w3.org/2000/svg','path');
+    // <path d="M 5 6   A 6 6    0 0 1   1 1"
+    //       stroke="green"
+    //       stroke-width=".1" fill-opacity="0"/>
+
+    arc.setAttribute('d', "M 5 6   A 6 6    0 0 1   1 1");
+    arc.setAttribute("stroke", "green");
+    g.setAttribute("stroke-width", .1);
+    g.setAttribute("fill-opacity", 0);
+    g.append(arc);
+
+    for (const [type, x1, y1, x2, y2] of wire.edges) {
 
         // TODO check namespace url
-        let newLine = document.createElementNS('http://www.w3.org/2000/svg','line');
-        newLine.setAttribute('x1',x1);
-        newLine.setAttribute('y1',y1);
-        newLine.setAttribute('x2',x2);
-        newLine.setAttribute('y2',y2);
-        newLine.setAttribute("stroke", type === "Arc" ? "red" : "black");
-        newLine.setAttribute("stroke-width", ".1");
-        g.append(newLine);
+        let line = document.createElementNS('http://www.w3.org/2000/svg','line');
+        line.setAttribute('x1',x1);
+        line.setAttribute('y1',y1);
+        line.setAttribute('x2',x2);
+        line.setAttribute('y2',y2);
+        line.setAttribute("stroke", type === "Arc" ? "red" : "black");
+        line.setAttribute("stroke-width", ".1");
+        g.append(line);
     }
 }
 
